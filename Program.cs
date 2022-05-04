e7a18e2e6e5bfa882c7b039e950a083b28936059
Kol <62248045+Kol-Dev@users.noreply.github.com>
2022-05-04 17:28:46 +0200

    Add files via upload

--- Program.cs
+++ Program.cs
@@ -753,16 +753,20 @@ namespace CSharp_CheatSheet
                     break;
                 case 3: // Grouping Operators (GroupBy ToLookup)
                     Space();
-                    Console.WriteLine("Which LINQ Grouping Operator ? \n1) GroupBy");
+                    Console.WriteLine("Which LINQ Grouping Operator ? \n1) GroupBy (Query Syntax)\n2) GroupBy (Method Syntax)");
                     var InputGroupingOperator = Console.ReadLine();
                     int valueInputGroupingOperator = int.Parse(InputGroupingOperator);
                     switch (valueInputGroupingOperator)
                     {
                         case 1:
-                            // GroupBy
-                            LINQGroupByFunction(studentList);
+                            // GroupBy Query
+                            LINQGroupByFunctionQuery(studentList);
                             break;
                         case 2:
+                            // GroupBy Method
+                            LINQGroupByFunctionMethod(studentList);
+                            break;
+                        case 3:
                             // ToLookUp
                             LINQToLookUpFunction(studentList);
                             break;
@@ -938,7 +942,7 @@ namespace CSharp_CheatSheet
                 Console.WriteLine("Name: {0}, Age:{1}, ID:{2}", std.StudentName, std.Age, std.StudentID);
         }
 
-        public static void LINQGroupByFunction(IList<Student> studentList){
+        public static void LINQGroupByFunctionQuery(IList<Student> studentList){
             var groupedAgeResult = from s in studentList group s by s.Age;
             var groupedNameResult = from s in studentList group s by s.StudentName;
             var groupedIDResult = from s in studentList group s by s.StudentID;
@@ -966,6 +970,37 @@ namespace CSharp_CheatSheet
             {
                 Console.WriteLine("-- ID Group: {0}", ageGroup.Key); 
                 foreach(Student s in ageGroup)
+                    Console.WriteLine("Student Name: {0}", s.StudentName);
+            }
+        }
+
+        public static void LINQGroupByFunctionMethod(IList<Student> studentList){
+            var groupedResultAge = studentList.GroupBy(s => s.Age);
+            var groupedResultID = studentList.GroupBy(s => s.StudentID);
+            var groupedResultName = studentList.GroupBy(s => s.StudentName);
+
+            Space();
+            Console.WriteLine("----- Age Groups -----");
+            foreach (var ageGroup in groupedResultAge)
+            {
+                Console.WriteLine("-- Age Group: {0}", ageGroup.Key);  // Each group has a key
+                foreach(Student s in ageGroup)  // Each group has a inner collection  
+                    Console.WriteLine("Student Name: {0}", s.StudentName);
+            }
+            Space();
+            Console.WriteLine("----- ID Groups -----");
+            foreach (var IDGroup in groupedResultID)
+            {
+                Console.WriteLine("-- ID Group: {0}", IDGroup.Key);
+                foreach(Student s in IDGroup) 
+                    Console.WriteLine("Student Name: {0}", s.StudentName);
+            }
+            Space();
+            Console.WriteLine("----- Name Groups -----");
+            foreach (var NameGroup in groupedResultName)
+            {
+                Console.WriteLine("-- Name Group: {0}", NameGroup.Key);
+                foreach(Student s in NameGroup) 
                     Console.WriteLine("Student Name: {0}", s.StudentName);
             }
         }
