eb4cdb7d760ccb18bd88a05e444469f668a5dece
Kol <62248045+Kol-Dev@users.noreply.github.com>
2022-05-05 17:32:22 +0200

    Add files via upload

--- Program.cs
+++ Program.cs
@@ -756,11 +756,11 @@ namespace CSharp_CheatSheet
                     {
                         case 1:
                             // GroupBy Query
-                            LINQGroupByFunctionQuery(studentList);
+                            GroupByOperator.LINQGroupByFunctionQuery(studentList);
                             break;
                         case 2:
                             // GroupBy Method
-                            LINQGroupByFunctionMethod(studentList);
+                            GroupByOperator.LINQGroupByFunctionMethod(studentList);
                             break;
                         case 3:
                             // ToLookUp
@@ -938,68 +938,9 @@ namespace CSharp_CheatSheet
                 Console.WriteLine("Name: {0}, Age:{1}, ID:{2}", std.StudentName, std.Age, std.StudentID);
         }
 
-        public static void LINQGroupByFunctionQuery(IList<Student> studentList){
-            var groupedAgeResult = from s in studentList group s by s.Age;
-            var groupedNameResult = from s in studentList group s by s.StudentName;
-            var groupedIDResult = from s in studentList group s by s.StudentID;
+        
 
-            //iterate each group  
-            Space();
-            Console.WriteLine("----- Group by Age -----");
-            foreach (var ageGroup in groupedAgeResult)
-            {
-                Console.WriteLine("-- Age Group: {0}", ageGroup.Key); // Each group has a key 
-                foreach(Student s in ageGroup) // Each group has inner collection
-                    Console.WriteLine("Student Name: {0}", s.StudentName);
-            }
-            Space();
-            Console.WriteLine("----- Group by Name -----");
-            foreach (var ageGroup in groupedNameResult)
-            {
-                Console.WriteLine("-- Name Group: {0}", ageGroup.Key);
-                foreach(Student s in ageGroup)
-                    Console.WriteLine("Student Name: {0}", s.StudentName);
-            }
-            Space();
-            Console.WriteLine("----- Group by ID -----");
-            foreach (var ageGroup in groupedIDResult)
-            {
-                Console.WriteLine("-- ID Group: {0}", ageGroup.Key); 
-                foreach(Student s in ageGroup)
-                    Console.WriteLine("Student Name: {0}", s.StudentName);
-            }
-        }
-
-        public static void LINQGroupByFunctionMethod(IList<Student> studentList){
-            var groupedResultAge = studentList.GroupBy(s => s.Age);
-            var groupedResultID = studentList.GroupBy(s => s.StudentID);
-            var groupedResultName = studentList.GroupBy(s => s.StudentName);
-
-            Space();
-            Console.WriteLine("----- Age Groups -----");
-            foreach (var ageGroup in groupedResultAge)
-            {
-                Console.WriteLine("-- Age Group: {0}", ageGroup.Key);  // Each group has a key
-                foreach(Student s in ageGroup)  // Each group has a inner collection  
-                    Console.WriteLine("Student Name: {0}", s.StudentName);
-            }
-            Space();
-            Console.WriteLine("----- ID Groups -----");
-            foreach (var IDGroup in groupedResultID)
-            {
-                Console.WriteLine("-- ID Group: {0}", IDGroup.Key);
-                foreach(Student s in IDGroup) 
-                    Console.WriteLine("Student Name: {0}", s.StudentName);
-            }
-            Space();
-            Console.WriteLine("----- Name Groups -----");
-            foreach (var NameGroup in groupedResultName)
-            {
-                Console.WriteLine("-- Name Group: {0}", NameGroup.Key);
-                foreach(Student s in NameGroup) 
-                    Console.WriteLine("Student Name: {0}", s.StudentName);
-            }
-        }
+        
 
         public static void LINQToLookUpFunction(IList<Student> studentList){
 
