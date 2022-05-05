c7d428ba015e522ae542174b65de32716402eeeb
Kol <62248045+Kol-Dev@users.noreply.github.com>
2022-05-05 16:59:52 +0200

    Add files via upload

--- Data Classes/Student.cs
+++ Data Classes/Student.cs
@@ -1,1 +1,14 @@
+using System;
+using System.Collections.Generic;
+using System.Text;
 
+namespace CSharp_CheatSheet
+{
+    public class Student{
+            public int StudentID { get; set; }
+            public string StudentName { get; set; }
+            public int Age { get; set; }
+        }
+
+        
+}
